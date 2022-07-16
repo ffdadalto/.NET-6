6ea988592f9d5a24e2670756dd52e08b4f0ad7af
ffdadalto <ffdadalto@gmail.com>
2022-07-15 21:16:29 -0300

    addDescriptionOnProducts

---
+++ Migrations/20220716001341_DescriptionOnProducts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_6.Migrations
+{
+    public partial class DescriptionOnProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+        }
+    }
+}
