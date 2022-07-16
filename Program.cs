fa1d1cfdaf41310b9c499bb09e8a27e475253cf6
ffdadalto <ffdadalto@gmail.com>
2022-07-15 21:44:54 -0300

    RequireTags

--- Program.cs
+++ Program.cs
@@ -81,6 +81,20 @@ public class Product
     public string Code { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<Tag> Tags { get; set; }
+}
+
+public class Category{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Tag{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ProductId { get; set; }
 }
 
 public class ApplicationDbContext : DbContext
